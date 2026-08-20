f30aa498ba65a9500e07631f6c8f50569eda9153
Flydown98 <qazw1385@gmail.com>
2026-08-20 11:46:27 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.CAMPAIGN_APP_CONFIG = {
   organization: '남양주시장애인복지관',
   campaignName: '불을 끄고 별을 켜다',
-  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbw68mHjFb7X4NKIMCZB0p6xRVaIplK_TmlscKsMS5UK6oYqsEZKhow5DmBjYPSDVQHFLw/exec',
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbzHvWpEkIiCQb2iE49MQgdTFvz816c1GNXI2dp_pdG85T0FHhar-ZET0Z7GM9xDvKnS5Q/exec',
   privacyPolicyUrl: 'https://nyjwel.or.kr/privacy',
   instagramHandle: '@nyjwel',
   shareHashtags: [
